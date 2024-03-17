675922d2c4ab8006239955a3b06a02d9ac925786
vanillovin <vanillovin@gmail.com>
2024-03-17 16:59:58 +0900

    feat: onChange와 input type=radio로 평점 구현하기
    - onKeyDown이나 onClick을 쓰지 않고 onChange와 input type=radio만 써서 구현하기.
    - 클릭하면 선택되고 스타일이 바뀌어야 하고, 방향 키로 좌우 이동이 가능해야 합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,11 @@
-import { useState } from 'react';
-import { css } from '../styled-system/css';
+import OnChangeRadioInput from './components/task/OnChangeRadioInput';
 
 function App() {
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedValue(event.target.value);
-  };
-
   return (
-    <div>
-      <div
-        className={css({
-          fontSize: '2xl',
-        })}
-      >
-        div
-      </div>
-      <fieldset className={css({})}>
-        <legend>
-          클래스에 대해 솔직한 의견을 주시면, 강사님에게 전달하여 개선하도록
-          하겠습니다
-        </legend>
-        {[1, 2, 3, 4, 5].map((value) => (
-          <div
-            key={value}
-            className={css({
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            })}
-          >
-            <label
-              htmlFor={`radio${value}`}
-              className={css({
-                display: 'absolute',
-                cursor: 'pointer',
-                color: selectedValue === value.toString() ? 'white' : 'black',
-              })}
-            >
-              {value}
-            </label>
-            <input
-              id={`radio${value}`}
-              type="radio"
-              className={`w-16 h-16 appearance-none border rounded-full shadow-md 
-              checked:bg-blue-400 checked:border-transparent focus:outline-none 
-              ${
-                selectedValue === value.toString() ? 'checked:bg-blue-400' : ''
-              }`}
-              value={value}
-              name="rating"
-              onChange={handleChange}
-            />
-          </div>
-        ))}
-      </fieldset>
-    </div>
+    <>
+      <OnChangeRadioInput />
+    </>
   );
 }
 
 export default App;
-
-const styles = css({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  rowGap: 2,
-});
